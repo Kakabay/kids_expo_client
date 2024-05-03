e083ab27afea33aa8d580416caae58c34cf7ee5d
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-03 18:43:16 +0500

    news fetched

--- src/components/Home/NewsCard.tsx
+++ src/components/Home/NewsCard.tsx
@@ -1,27 +1,26 @@
 import { Link } from 'react-router-dom';
 
 interface IProps {
-  img: string;
-  date: string;
-  text: string;
-  mobile?: boolean;
+  title: string;
+  published_at: string;
+  path: string;
 }
 
-export const NewsCard = ({ img, text, date }: IProps) => {
+export const NewsCard = ({ title, published_at, path }: IProps) => {
   return (
     <div className="bg-bgWhite rounded-sm mx-auto sm:max-w-[290px] w-full transition-all hover:hover-shadow cursor-pointer h-full">
       <Link to={''} className="h-full">
         {/* Aspect ration 1.8:1 */}
         <img
-          src={img}
+          src={path}
           // width={mobile ? 620 : 290}
           // height={mobile ? 400 : 160}
           alt="photo"
           className="h-[160px] w-full object-cover mb-[25px]"
         />
         <div className=" h-full sm:h-[137px]">
-          <p className="font-bold leading-[125%] text-[16px] news-text mb-[10px]">{text}</p>
-          <p className="text-extraSm leading-[125%] text-gray">{date}</p>
+          <p className="font-bold leading-[125%] text-[16px] news-text mb-[10px]">{title}</p>
+          <p className="text-extraSm leading-[125%] text-gray">{published_at}</p>
         </div>
       </Link>
     </div>
