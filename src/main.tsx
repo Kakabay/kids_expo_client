a563bb369df87d5843da4cc0308eb269a5282fd1
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-08 16:52:48 +0500

    added path for formPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,40 @@
 // Mo
-import React from 'react';
+import React from "react";
 // Styles
-import './index.css';
+import "./index.css";
 // ReactDOM
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // Components
-import App from './App.tsx';
+import App from "./App.tsx";
 // Tanstack query
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // Pages
-import FaqPage from './pages/FaqPage.tsx';
-import HomePage from './pages/HomePage.tsx';
-import ExhibitionAbout from './pages/exhibition/ExhibitionAbout.tsx';
+import FaqPage from "./pages/FaqPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import ExhibitionAbout from "./pages/exhibition/ExhibitionAbout.tsx";
+import ExhibitionForm from "./pages/exhibition/ExhibitionForm.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <HomePage />,
       },
       {
-        path: '/faq',
+        path: "/faq",
         element: <FaqPage />,
       },
       {
-        path: '/about-exebition',
+        path: "/about-exhibition",
         element: <ExhibitionAbout />,
+      },
+      {
+        path: "/participants-exhibition",
+        element: <ExhibitionForm />,
       },
     ],
   },
@@ -37,10 +42,10 @@ const router = createBrowserRouter([
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
