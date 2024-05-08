1a0d100edef4f800b2e06c6e61a3615da0a4f4c0
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-08 16:53:05 +0500

    added pathnames for participants page

--- src/components/global/Sidebar.tsx
+++ src/components/global/Sidebar.tsx
@@ -1,8 +1,8 @@
-import clsx from 'clsx';
+import clsx from "clsx";
 
-import { sidebarData } from '../../database/pathnames';
-import { v4 } from 'uuid';
-import { Link, useLocation } from 'react-router-dom';
+import { sidebarData } from "../../database/pathnames";
+import { v4 } from "uuid";
+import { Link, useLocation } from "react-router-dom";
 
 export const Sidebar = () => {
   const pathname = useLocation().pathname;
@@ -10,10 +10,18 @@ export const Sidebar = () => {
   return (
     <aside className="flex flex-col items-start gap-y-[12px] py-[20px] sticky top-0 left-0 overflow-hidden min-w-[230px]">
       {sidebarData
-        .filter((obj) => pathname === '/about-exebition' && obj.exebition)
+        .filter(
+          (obj) =>
+            (pathname === "/about-exhibition" && obj.exhibition) ||
+            (pathname === "/participants-exhibition" && obj.members)
+        )
         .map((item) => (
           <div key={v4()}>
-            <p className={'text-bgWhite mb-[16px] text-[16px] font-bold leading-[1.5]'}>
+            <p
+              className={
+                "text-bgWhite mb-[16px] text-[16px] font-bold leading-[1.5]"
+              }
+            >
               {item.pathname}
             </p>
             <div className="flex flex-col items-start gap-y-[8px]">
@@ -22,12 +30,14 @@ export const Sidebar = () => {
                   <Link
                     to={obj.link}
                     className={clsx(
-                      'cursor-pointer py-1 leading-[130%] text-[14px] transition-all hover:text-purple',
+                      "cursor-pointer py-1 leading-[130%] text-[14px] transition-all hover:text-purple",
                       {
-                        'hover:text-purple text-purple hover:cursor-default': obj.link === pathname,
-                      },
+                        "hover:text-purple text-purple hover:cursor-default":
+                          obj.link === pathname,
+                      }
                     )}
-                    key={v4()}>
+                    key={v4()}
+                  >
                     {obj.title}
                   </Link>
                 ))}
