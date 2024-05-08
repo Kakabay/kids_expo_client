ea1ea49aaa3ab73fe7f1e022f86e3acd9981b12e
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-08 15:18:40 +0500

    dynamic mb added

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,25 @@
-import clsx from 'clsx';
+import clsx from "clsx";
 
-export const Button = ({ text, news }: { text: string; news?: boolean }) => {
+export const Button = ({
+  text,
+  news,
+  little,
+}: {
+  text: string;
+  news?: boolean;
+  little?: boolean;
+}) => {
   return (
     <div
       className={clsx(
-        'cursor-pointer w-fit mx-auto py-[17px] px-[50px] text-[14px] text-purple border-[1px] border-purple rounded-sm hover:text-white hover:bg-purple transition-all',
-        { 'mt-10': news },
-      )}>
+        "cursor-pointer w-fit font-medium mx-auto py-[17px] text-[14px] text-purple border-[1px] border-purple rounded-sm hover:text-white hover:bg-purple transition-all",
+        {
+          "mt-10": news,
+          "px-[16px]": little,
+          "px-[50px]": !little,
+        }
+      )}
+    >
       {text}
     </div>
   );
