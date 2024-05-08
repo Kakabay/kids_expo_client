574dc40d1b8caf8dcecbee1c2361b519fc69a060
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-08 16:53:42 +0500

    added optional mb32

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,10 +1,19 @@
 import clsx from "clsx";
 
-export const Title = ({ title, mb24 }: { title: string; mb24?: boolean }) => {
+export const Title = ({
+  title,
+  mb24,
+  mb32,
+}: {
+  title: string;
+  mb24?: boolean;
+  mb32?: boolean;
+}) => {
   return (
     <h1
       className={clsx("text-[34px] font-semibold leading-[100%] text-black1", {
         "mb-6": mb24,
+        "mb-8": mb32,
       })}
     >
       {title}
