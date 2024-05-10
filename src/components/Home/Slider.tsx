061c40c9f93b09ed3af7de9a406e1c2d491c49e4
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-10 19:46:14 +0500

    banners translations added

--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -30,6 +30,23 @@ export const Slider = () => {
         loop
         speed={1500}
         autoplay={{ delay: 5000 }}>
+        {/* {bannersData
+          ? bannersData.map((item) =>
+              item.code.includes('main-banner') ? (
+                <SwiperSlide key={v4()}>
+                  <Link to={''}>
+                    <div className="h-[490px] w-full">
+                      <img
+                        src={item.banner_items[0].image}
+                        alt={item.banner_items[0].title}
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                  </Link>
+                </SwiperSlide>
+              ) : null,
+            )
+          : null} */}
         {localization === 'en' ? (
           <SwiperSlide key={v4()}>
             <Link to={''}>
@@ -63,14 +80,14 @@ export const Slider = () => {
         )}
 
         {/* <div className="container absolute right-0 bottom-[25px] swiper-pagination swiper-pagination-horizontal">
-            <div className="swiper-pagination-fraction items-center justify-end gap-4">
-              <div className="autoplay-progress w-[200px] flex items-center gap-5 bg-bgWhite rounded-full">
-                <svg ref={progressCircle}>
-                  <line viewBox="0 0 63 5" x1={0} x2={63} />
-                </svg>
-              </div>
+          <div className="swiper-pagination-fraction items-center justify-end gap-4">
+            <div className="autoplay-progress w-[200px] flex items-center gap-5 bg-bgWhite rounded-full">
+              <svg ref={progressCircle}>
+                <line viewBox="0 0 63 5" x1={0} x2={63} />
+              </svg>
             </div>
-          </div> */}
+          </div>
+        </div> */}
       </Swiper>
     );
   }
