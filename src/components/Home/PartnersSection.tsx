875d770b01c4c2f3b4f6711314cf6609eafbb761
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-27 17:53:21 +0500

    swiper fixed

--- src/components/Home/PartnersSection.tsx
+++ src/components/Home/PartnersSection.tsx
@@ -1,9 +1,9 @@
-import { Title } from "../ui/Title";
-import { useGetEn } from "../../hooks/language/useGetEn";
-import useGetPartners from "../../hooks/useGetPartners";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import { v4 } from "uuid";
+import { Title } from '../ui/Title';
+import { useGetEn } from '../../hooks/language/useGetEn';
+import useGetPartners from '../../hooks/useGetPartners';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import { v4 } from 'uuid';
 
 export const PartnersSection = () => {
   const { partnersData } = useGetPartners();
@@ -12,27 +12,27 @@ export const PartnersSection = () => {
 
   return (
     <div className="container mb-[50px]">
-      <Title title={useGetEn("Partners", "Партнёры")} />
+      <Title title={useGetEn('Partners', 'Партнёры')} />
 
       <Swiper
         modules={[Autoplay]}
         autoplay={{ delay: 0 }}
         speed={5000}
         spaceBetween={45}
+        slidesPerGroup={4}
         slidesPerView={2.5}
         loop
         breakpoints={{
           768: { slidesPerView: 4 },
           630: { slidesPerView: 3 },
-        }}
-      >
+        }}>
         {partnersData
           ? partnersData.map((item) =>
               item.images[0].path ? (
-                <SwiperSlide>
+                <SwiperSlide key={v4()}>
                   <img src={item.images[0].path} alt="" />
                 </SwiperSlide>
-              ) : null
+              ) : null,
             )
           : null}
       </Swiper>
