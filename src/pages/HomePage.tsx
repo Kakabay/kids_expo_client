f65ca89c2c7399e9fe26220f68415e9fa1f22589
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-07-04 17:21:05 +0500

    commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -67,6 +67,17 @@ export default function HomePage() {
 
       <PartnersSection />
 
+      <section className="mb-[50px] bg-[#f9f9f9]">
+        <div className="flex py-5 justify-center container">
+          <a href="https://exposale.net/ru" target="_blank">
+            <img
+              src="https://exposale.net/template-admin/assets/elFinder/files/banners/728x90rus.png"
+              title="Найди свою выставку на EXPOSALE.net "
+            />
+          </a>
+        </div>
+      </section>
+
       <section className="relative w-full h-[300px] sm:h-[350px] md:h-[400px] lg:h-[480px] mb-[50px]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3148.67827952586!2d58.29659607507902!3d37.8912058554459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f7003944259cb1d%3A0xafc893357e4b0d2!2z0KLQvtGA0LPQvtCy0L4t0L_RgNC-0LzRi9GI0LvQtdC90L3QsNGPINC_0LDQu9Cw0YLQsCDQotGD0YDQutC80LXQvdC40YHRgtCw0L3QsA!5e0!3m2!1sru!2s!4v1713164734635!5m2!1sru!2s"
