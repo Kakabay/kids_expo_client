d03ee3e3079426282c37371ac52e024876bf3ad4
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-08-22 16:56:05 +0500

    commit

--- src/components/Home/BurgerMenu.tsx
+++ src/components/Home/BurgerMenu.tsx
@@ -1,3 +1,166 @@
+import { burgerData, burgerData2 } from "@/database/burger.data";
+import { AnimatePresence, motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Separator } from "../../../components/ui/separator";
+import { useBurger } from "@/services/zustand/zusBurger";
+import { useLang } from "../News/zusLang";
+import { activeLangType } from "../Header/LangMenu";
+
+const burgerLangs: activeLangType[] = [
+  // {
+  //   title: "Tm",
+  //   localization: "tm",
+  // },
+  {
+    title: "Ру",
+    localization: "ru",
+  },
+  {
+    title: "En",
+    localization: "en",
+  },
+];
+
+const BurgerMenu = () => {
+  const [activeDrop, setActiveDrop] = useState("");
+
+  const [activeFooter, setActiveFooter] = useState("");
+
+  const burger = useBurger((state) => state.burger);
+  const setBurger = useBurger((state) => state.setBurger);
+
+  const setLang = useLang((state) => state.setLang);
+
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => document.body.classList.remove("overflow-hidden");
+  }, []);
+
+  const findItem = burgerData.find((item) => item.view === activeDrop);
+
+  const findItem2 = burgerData2.find((item) => item.view === activeFooter);
+
+  return (
+    <motion.div
+      initial={{ x: "100%", opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: "100%", opacity: 0 }}
+      transition={{ ease: "circInOut", duration: 0.3 }}
+      className="w-screen h-screen top-[97px] bg-purple absolute leading-[120%] text-white left-0 py-10 overflow-auto"
+    >
+      <motion.div className="container flex flex-col gap-5 font-normal">
+        <AnimatePresence>
+          {!findItem ? (
+            burgerData.map((item, i) => (
+              <motion.div key={i}>
+                {item.link ? (
+                  <motion.div>
+                    <Link onClick={() => setBurger(false)} to={item.link}>
+                      {item.view}
+                    </Link>
+                  </motion.div>
+                ) : (
+                  <div
+                    key={i}
+                    onClick={() => setActiveDrop(item.view)}
+                    className="flex items-center justify-between"
+                  >
+                    {item.view}
+                    <ChevronRight size={20} />
+                  </div>
+                )}
+              </motion.div>
+            ))
+          ) : (
+            <motion.div className="cursor-pointer">
+              <div
+                onClick={() => setActiveDrop("")}
+                className="flex items-center gap-3 pb-2.5"
+              >
+                <ChevronRight size={20} className="rotate-[180deg]" />
+                {findItem.view}
+              </div>
+
+              <Separator className="container mt-2.5" />
+
+              <div className="flex flex-col gap-5 mt-5">
+                {findItem.info?.map((item, i) => (
+                  <Link key={i} onClick={() => setBurger(false)} to={item.link}>
+                    {item.view}
+                  </Link>
+                ))}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+
+      <div className="container my-10">
+        <Separator className="h-[0.3px]" />
+      </div>
+
+      <div className="flex flex-col gap-5 container">
+        {!findItem2 ? (
+          burgerData2.map((item, i) => (
+            <div
+              key={i}
+              onClick={() => setActiveFooter(item.view)}
+              className="flex items-center justify-between"
+            >
+              {item.view}
+              <ChevronRight size={20} />
+            </div>
+          ))
+        ) : (
+          <div className="cursor-pointer">
+            <div
+              onClick={() => setActiveFooter("")}
+              className="flex items-center gap-3 pb-2.5"
+            >
+              <ChevronRight size={20} className="rotate-[180deg]" />{" "}
+              {findItem2.view}
+            </div>
+            <Separator className="container mt-2.5" />
+
+            <div className="flex flex-col gap-5 mt-5">
+              {findItem2.info?.map((item, i) => (
+                <Link key={i} onClick={() => setBurger(false)} to={item.link}>
+                  {item.view}
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="flex items-center justify-center mt-20 mx-auto gap-10">
+        {burgerLangs.map((item, i) => (
+          <div
+            key={i}
+            onClick={() => {
+              setLang(item);
+              setBurger(false);
+            }}
+            className="flex cursor-pointer items-center gap-[10px]"
+          >
+            <img
+              src={`/assets/icons/burgerMenu/${item.localization}.svg`}
+              alt="flag"
+            />
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default BurgerMenu;
+
+//       <div className="flex items-center mx-auto gap-10">
 // import { motion } from 'framer-motion';
 // import { headerMenu, headerMenu2 } from '../../database/header.data';
 // import { Link } from 'react-router-dom';
@@ -13,21 +176,6 @@
 //   const chooseDataLang = (en: string, ru: string) => (localization === 'en' ? en : ru);
 
 //   const [activeMenu, setActiveMenu] = useState<string>('');
-
-//   const burgerLangs: activeLangType[] = [
-//     // {
-//     //   title: "Tm",
-//     //   localization: "tm",
-//     // },
-//     {
-//       title: 'Ру',
-//       localization: 'ru',
-//     },
-//     {
-//       title: 'En',
-//       localization: 'en',
-//     },
-//   ];
 
 //   useEffect(() => {
 //     document.body.classList.add('overflow-hidden');
@@ -157,112 +305,3 @@
 //     </motion.div>
 //   );
 // };
-
-import { burgerData, burgerData2 } from '@/database/burger.data';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
-import { ChevronRight } from 'lucide-react';
-import { useEffect, useState } from 'react';
-import { Separator } from '../../../components/ui/separator';
-import { useBurger } from '@/services/zustand/zusBurger';
-import { useLang } from '../News/zusLang';
-
-const BurgerMenu = () => {
-  const [activeDrop, setActiveDrop] = useState('');
-
-  const [activeFooter, setActiveFooter] = useState('');
-
-  const burger = useBurger((state) => state.burger);
-  const setBurger = useBurger((state) => state.setBurger);
-
-  const setLang = useLang((state) => state.setLang);
-
-  useEffect(() => {
-    document.body.classList.add('overflow-hidden');
-
-    return () => document.body.classList.remove('overflow-hidden');
-  }, []);
-
-  const findItem = burgerData.find((item) => item.view === activeDrop);
-
-  const findItem2 = burgerData2.find((item) => item.view === activeFooter);
-
-  return (
-    <motion.div
-      initial={{ x: '100%', opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: '100%', opacity: 0 }}
-      transition={{ ease: 'circInOut', duration: 0.3 }}
-      className="w-screen h-screen top-[97px] bg-purple absolute leading-[120%] text-white left-0 py-10 overflow-auto">
-      <div className="container flex flex-col gap-5 font-normal">
-        {!findItem ? (
-          burgerData.map((item, i) => (
-            <div key={i}>
-              {item.link ? (
-                <Link key={i} to={item.link}>
-                  {item.view}
-                </Link>
-              ) : (
-                <div
-                  key={i}
-                  onClick={() => setActiveDrop(item.view)}
-                  className="flex items-center justify-between">
-                  {item.view}
-                  <ChevronRight size={20} />
-                </div>
-              )}
-            </div>
-          ))
-        ) : (
-          <div>
-            <div onClick={() => setActiveDrop('')} className="flex items-center gap-3 pb-2.5">
-              <ChevronRight size={20} className="rotate-[180deg]" /> {findItem.view}
-            </div>
-            <Separator className="container mt-2.5" />
-
-            <div className="flex flex-col gap-5 mt-5">
-              {findItem.info?.map((item) => (
-                <Link onClick={() => setBurger(false)} to={item.link}>
-                  {item.view}
-                </Link>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-
-      <Separator className="my-10 px-4 container" />
-
-      <div className="flex flex-col gap-5 container">
-        {!findItem2 ? (
-          burgerData2.map((item, i) => (
-            <div
-              key={i}
-              onClick={() => setActiveFooter(item.view)}
-              className="flex items-center justify-between">
-              {item.view}
-              <ChevronRight size={20} />
-            </div>
-          ))
-        ) : (
-          <div>
-            <div onClick={() => setActiveFooter('')} className="flex items-center gap-3 pb-2.5">
-              <ChevronRight size={20} className="rotate-[180deg]" /> {findItem2.view}
-            </div>
-            <Separator className="container mt-2.5" />
-
-            <div className="flex flex-col gap-5 mt-5">
-              {findItem2.info?.map((item) => (
-                <Link onClick={() => setBurger(false)} to={item.link}>
-                  {item.view}
-                </Link>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    </motion.div>
-  );
-};
-
-export default BurgerMenu;
--- src/pages/media/Photo.tsx
+++ src/pages/media/Photo.tsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react';
-import { BreadCrumbs } from '../../components/ui/BreadCrumbs';
-import { Title } from '../../components/ui/Title';
+import { useEffect, useState } from "react";
+import { BreadCrumbs } from "../../components/ui/BreadCrumbs";
+import { Title } from "../../components/ui/Title";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselPrevious,
   CarouselNext,
-} from '../../../components/ui/carousel';
-import { type CarouselApi } from '../../../components/ui/carousel';
-import YearTab from '@/components/Video/YearTab';
-import useGetPhotos from '@/hooks/media/useGetPhotos';
-import Loader from '@/components/Loader';
+} from "../../../components/ui/carousel";
+import { type CarouselApi } from "../../../components/ui/carousel";
+import YearTab from "@/components/Video/YearTab";
+import useGetPhotos from "@/hooks/media/useGetPhotos";
+import Loader from "@/components/Loader";
 
 const items = [
   {
-    img: '/assets/images/photo.png',
+    img: "/assets/images/photo.png",
   },
   {
-    img: '/assets/images/photo.png',
+    img: "/assets/images/photo.png",
   },
   {
-    img: '/assets/images/photo.png',
+    img: "/assets/images/photo.png",
   },
   {
-    img: '/assets/images/photo.png',
+    img: "/assets/images/photo.png",
   },
 ];
 
@@ -39,7 +39,7 @@ const Photo = () => {
 
     setCurrent(api.selectedScrollSnap() + 1);
 
-    api.on('select', () => {
+    api.on("select", () => {
       setCurrent(api.selectedScrollSnap() + 1);
     });
   }, [api]);
@@ -48,20 +48,26 @@ const Photo = () => {
 
   return (
     <div className="">
-      <BreadCrumbs second={'Медиа'} path="/media/photo" third="Фоторепортаж" />
+      <BreadCrumbs second={"Медиа"} path="/media/photo" third="Фоторепортаж" />
       <Title title="Фоторепортаж" className="mb-8" />
 
-      <h4 className="mb-8 font-semibold text-[21px] leading-none">Экспозиция</h4>
+      <h4 className="mb-8 font-semibold text-[21px] leading-none">
+        Экспозиция
+      </h4>
 
       <YearTab />
 
       {data ? (
-        <Carousel setApi={setApi}>
+        <Carousel className="md:px-10" setApi={setApi}>
           <CarouselContent>
             {data ? (
               data.map((item, i) => (
-                <CarouselItem key={i} className="w-full">
-                  <img src={item.media_url} alt="" className="w-full h-full object-cover" />
+                <CarouselItem key={i} className="w-full max-h-[450px] ">
+                  <img
+                    src={item.media_url}
+                    alt=""
+                    className="w-full h-full object-cover overflow-hidden object-center"
+                  />
                 </CarouselItem>
               ))
             ) : (
@@ -71,12 +77,12 @@ const Photo = () => {
 
           <div className="flex items-center justify-between mt-4">
             <div className="text-black text-[14px]">
-              {current}/{items.length - 1}
+              {current}/{items.length + 1}
             </div>
 
             <div className="flex items-center gap-5">
-              <CarouselPrevious variant={'default'} />
-              <CarouselNext variant={'default'} />
+              <CarouselPrevious />
+              <CarouselNext />
             </div>
           </div>
         </Carousel>
