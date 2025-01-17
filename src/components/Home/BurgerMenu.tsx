456f95d874095adccff93143fa53221a118c8c4f
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-17 19:12:02 +0500

    burger animation changed

--- src/components/Home/BurgerMenu.tsx
+++ src/components/Home/BurgerMenu.tsx
@@ -3,10 +3,10 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { ChevronRight } from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { Separator } from '../../../components/ui/separator';
 import { useBurger } from '@/services/zustand/zusBurger';
 import { useLang } from '../News/zusLang';
 import { activeLangType } from '../Header/LangMenu';
+import { cn } from '@/lib/utils';
 
 const burgerLangs: activeLangType[] = [
   // {
@@ -24,10 +24,6 @@ const burgerLangs: activeLangType[] = [
 ];
 
 const BurgerMenu = () => {
-  const [activeDrop, setActiveDrop] = useState('');
-
-  const [activeFooter, setActiveFooter] = useState('');
-
   const setBurger = useBurger((state) => state.setBurger);
 
   const setLang = useLang((state) => state.setLang);
@@ -38,9 +34,12 @@ const BurgerMenu = () => {
     return () => document.body.classList.remove('overflow-hidden');
   }, []);
 
-  const findItem = burgerData.find((item) => item.view === activeDrop);
+  const [active, setActive] = useState(0);
 
-  const findItem2 = burgerData2.find((item) => item.view === activeFooter);
+  const handleClick = (id: number, activeId: number) => {
+    if (id === activeId) setActive(0);
+    else setActive(id);
+  };
 
   return (
     <motion.div
@@ -48,84 +47,83 @@ const BurgerMenu = () => {
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: '100%', opacity: 0 }}
       transition={{ duration: 0.4, ease: [0.55, 0, 0.1, 1] }}
-      className="w-screen h-screen top-[97px] bg-purple absolute leading-[120%] text-white left-0 py-10 overflow-auto">
-      <motion.div className="container flex flex-col gap-5 font-normal">
-        <AnimatePresence>
-          {!findItem ? (
-            burgerData.map((item, i) => (
-              <motion.div key={i}>
-                {item.link ? (
-                  <motion.div>
-                    <Link onClick={() => setBurger(false)} to={item.link}>
+      className="w-screen h-screen top-[95px] bg-purple absolute leading-[120%] text-white left-0 py-10 overflow-auto">
+      <nav className="mx-4">
+        <div className="flex flex-col gap-6 h-[150px] overflow-hidden">
+          {burgerData.map((item, i) =>
+            item.menu ? (
+              <div key={i}>
+                <div
+                  onClick={() => handleClick(item.id, active)}
+                  key={i}
+                  className={cn('flex items-center justify-between', item.id !== 1 && 'hidden')}>
+                  {item.view}
+                  <ChevronRight
+                    className={cn(active === item.id && 'rotate-90', 'transition-all')}
+                    size={18}
+                  />
+                </div>
+
+                {active === item.id && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    className={cn(
+                      'flex flex-col gap-6 h-[200px] transition-all border-t mt-3 pt-4 border-t-white/20',
+                    )}>
+                    {item.info?.map((item, i) => (
+                      <Link key={i} to={item.link}>
+                        {item.view}
+                      </Link>
+                    ))}
+                  </motion.div>
+                )}
+              </div>
+            ) : (
+              <Link onClick={() => setBurger(false)} key={i} to={item.link || ''}>
+                {item.view}
+              </Link>
+            ),
+          )}
+        </div>
+
+        <hr className="border-white/20 my-8" />
+
+        <div className={cn('flex h-[105px] flex-col gap-6')}>
+          {burgerData2.map((item, i) => (
+            <div key={i}>
+              <div
+                onClick={() => handleClick(item.id, active)}
+                className="flex items-center justify-between">
+                {item.view}
+                <ChevronRight size={18} />
+              </div>
+
+              {active === item.id && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.3 }}
+                  className={cn(
+                    'flex flex-col gap-6 transition-all border-y mt-3 py-4 border-y-white/20',
+                  )}>
+                  {item.info?.map((item, i) => (
+                    <Link onClick={() => setBurger(false)} key={i} to={item.link}>
                       {item.view}
                     </Link>
-                  </motion.div>
-                ) : (
-                  <div
-                    key={i}
-                    onClick={() => setActiveDrop(item.view)}
-                    className="flex items-center justify-between">
-                    {item.view}
-                    <ChevronRight size={20} />
-                  </div>
-                )}
-              </motion.div>
-            ))
-          ) : (
-            <motion.div className="cursor-pointer">
-              <div onClick={() => setActiveDrop('')} className="flex items-center gap-3 pb-2.5">
-                <ChevronRight size={20} className="rotate-[180deg]" />
-                {findItem.view}
-              </div>
-
-              <Separator className="container mt-2.5" />
-
-              <div className="flex flex-col gap-5 mt-5">
-                {findItem.info?.map((item, i) => (
-                  <Link key={i} onClick={() => setBurger(false)} to={item.link}>
-                    {item.view}
-                  </Link>
-                ))}
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </motion.div>
-
-      <div className="container my-10">
-        <Separator className="h-[0.3px]" />
-      </div>
-
-      <div className="flex flex-col gap-5 container">
-        {!findItem2 ? (
-          burgerData2.map((item, i) => (
-            <div
-              key={i}
-              onClick={() => setActiveFooter(item.view)}
-              className="flex items-center justify-between">
-              {item.view}
-              <ChevronRight size={20} />
+                  ))}
+                </motion.div>
+              )}
             </div>
-          ))
-        ) : (
-          <div className="cursor-pointer">
-            <div onClick={() => setActiveFooter('')} className="flex items-center gap-3 pb-2.5">
-              <ChevronRight size={20} className="rotate-[180deg]" /> {findItem2.view}
-            </div>
-            <Separator className="container mt-2.5" />
+          ))}
+        </div>
+      </nav>
 
-            <div className="flex flex-col gap-5 mt-5">
-              {findItem2.info?.map((item, i) => (
-                <Link key={i} onClick={() => setBurger(false)} to={item.link}>
-                  {item.view}
-                </Link>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-
-      <div className="flex items-center justify-center mt-20 mx-auto gap-10">
+      <div
+        className={cn(
+          'flex transition-all  items-center justify-center mt-20 mx-auto gap-10',
+          active > 4 && 'translate-y-[200px]',
+        )}>
         {burgerLangs.map((item, i) => (
           <div
             key={i}
@@ -133,7 +131,7 @@ const BurgerMenu = () => {
               setLang(item);
               setBurger(false);
             }}
-            className="flex cursor-pointer items-center gap-[10px]">
+            className="flex cursor-pointer transition-all items-center gap-[10px]">
             <img src={`/assets/icons/burgerMenu/${item.localization}.svg`} alt="flag" />
             <p>{item.title}</p>
           </div>
@@ -144,3 +142,103 @@ const BurgerMenu = () => {
 };
 
 export default BurgerMenu;
+
+{
+  /* <motion.div
+initial={{ x: '100%', opacity: 0 }}
+animate={{ x: 0, opacity: 1 }}
+exit={{ x: '100%', opacity: 0 }}
+transition={{ duration: 0.4, ease: [0.55, 0, 0.1, 1] }}
+className="w-screen h-screen top-[97px] bg-purple absolute leading-[120%] text-white left-0 py-10 overflow-auto">
+<motion.div className="container flex flex-col gap-5 font-normal">
+  <AnimatePresence>
+    {!findItem ? (
+      burgerData.map((item, i) => (
+        <motion.div key={i}>
+          {item.link ? (
+            <motion.div>
+              <Link onClick={() => setBurger(false)} to={item.link}>
+                {item.view}
+              </Link>
+            </motion.div>
+          ) : (
+            <div
+              key={i}
+              onClick={() => setActiveDrop(item.view)}
+              className="flex items-center justify-between">
+              {item.view}
+              <ChevronRight size={20} />
+            </div>
+          )}
+        </motion.div>
+      ))
+    ) : (
+      <motion.div className="cursor-pointer">
+        <div onClick={() => setActiveDrop('')} className="flex items-center gap-3 pb-2.5">
+          <ChevronRight size={20} className="rotate-[180deg]" />
+          {findItem.view}
+        </div>
+
+        <Separator className="container mt-2.5" />
+
+        <div className="flex flex-col gap-5 mt-5">
+          {findItem.info?.map((item, i) => (
+            <Link key={i} onClick={() => setBurger(false)} to={item.link}>
+              {item.view}
+            </Link>
+          ))}
+        </div>
+      </motion.div>
+    )}
+  </AnimatePresence>
+</motion.div>
+
+<div className="container my-10">
+  <Separator className="h-[0.3px]" />
+</div>
+
+<div className="flex flex-col gap-5 container">
+  {!findItem2 ? (
+    burgerData2.map((item, i) => (
+      <div
+        key={i}
+        onClick={() => setActiveFooter(item.view)}
+        className="flex items-center justify-between">
+        {item.view}
+        <ChevronRight size={20} />
+      </div>
+    ))
+  ) : (
+    <div className="cursor-pointer">
+      <div onClick={() => setActiveFooter('')} className="flex items-center gap-3 pb-2.5">
+        <ChevronRight size={20} className="rotate-[180deg]" /> {findItem2.view}
+      </div>
+      <Separator className="container mt-2.5" />
+
+      <div className="flex flex-col gap-5 mt-5">
+        {findItem2.info?.map((item, i) => (
+          <Link key={i} onClick={() => setBurger(false)} to={item.link}>
+            {item.view}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )}
+</div>
+
+<div className="flex items-center justify-center mt-20 mx-auto gap-10">
+  {burgerLangs.map((item, i) => (
+    <div
+      key={i}
+      onClick={() => {
+        setLang(item);
+        setBurger(false);
+      }}
+      className="flex cursor-pointer items-center gap-[10px]">
+      <img src={`/assets/icons/burgerMenu/${item.localization}.svg`} alt="flag" />
+      <p>{item.title}</p>
+    </div>
+  ))}
+</div>
+</motion.div> */
+}
--- src/database/burger.data.ts
+++ src/database/burger.data.ts
@@ -1,105 +1,128 @@
 export const burgerData = [
   {
-    view: "Медиа",
-    viewEn: "Media",
+    view: 'Медиа',
+    viewEn: 'Media',
+    menu: true,
+    id: 1,
     info: [
       {
-        view: "Фоторепортаж",
-        viewEn: "Фоторепортаж",
-        link: "/media/photo",
+        view: 'Фоторепортаж',
+        viewEn: 'Фоторепортаж',
+        link: '/media/photo',
       },
       {
-        view: "Видеорепортаж",
-        viewEn: "Видеорепортаж",
-        link: "/media/video",
+        view: 'Видеорепортаж',
+        viewEn: 'Видеорепортаж',
+        link: '/media/video',
       },
     ],
   },
   {
-    view: "Новости",
-    viewEn: "News",
-    link: "/news",
+    menu: false,
+
+    id: 2,
+    view: 'Новости',
+    viewEn: 'News',
+    link: '/news',
   },
   {
-    view: "FAQ",
-    viewEn: "FAQ",
-    link: "/faq",
+    menu: false,
+    id: 3,
+
+    view: 'FAQ',
+    viewEn: 'FAQ',
+    link: '/faq',
   },
   {
-    view: "Контакты",
-    viewEn: "Contacts",
-    link: "/contacts",
+    menu: false,
+    id: 4,
+
+    view: 'Контакты',
+    viewEn: 'Contacts',
+    link: '/contacts',
   },
 ];
 
 export const burgerData2 = [
   {
-    view: "Выставка",
-    viewEn: "Exhibition",
+    id: 5,
+
+    view: 'Выставка',
+    viewEn: 'Exhibition',
+    menu: true,
+
     info: [
       {
-        view: "О выставке",
-        viewEn: "About",
-        link: "/exhibition-about",
+        view: 'О выставке',
+        viewEn: 'About',
+        link: '/exhibition-about',
       },
       {
-        view: "Тематика",
-        viewEn: "Theme",
-        link: "/exhibition-theme",
+        view: 'Тематика',
+        viewEn: 'Theme',
+        link: '/exhibition-theme',
       },
       {
-        view: "Место проведения",
-        viewEn: "Venue",
-        link: "/exhibition-avenue",
+        view: 'Место проведения',
+        viewEn: 'Venue',
+        link: '/exhibition-avenue',
       },
       {
-        view: "Информационная поддержка",
-        viewEn: "Information support",
-        link: "/exhibition-support",
+        view: 'Информационная поддержка',
+        viewEn: 'Information support',
+        link: '/exhibition-support',
       },
     ],
   },
   {
-    view: "Участникам",
-    viewEn: "Participants",
+    id: 6,
+
+    view: 'Участникам',
+    viewEn: 'Participants',
+    menu: true,
+
     info: [
       {
-        view: "Информация для участников",
-        viewEn: "Information for participants",
-        link: "/participants-info",
+        view: 'Информация для участников',
+        viewEn: 'Information for participants',
+        link: '/participants-info',
       },
       {
-        view: "Преимущества участия",
-        viewEn: "Benefits of participation",
-        link: "/participants-benefits",
+        view: 'Преимущества участия',
+        viewEn: 'Benefits of participation',
+        link: '/participants-benefits',
       },
       {
-        view: "Онлайн-заявка для участников",
-        viewEn: "Application for participation",
-        link: "/participants-apply",
+        view: 'Онлайн-заявка для участников',
+        viewEn: 'Application for participation',
+        link: '/participants-apply',
       },
       {
-        view: "Документы",
-        viewEn: "Documents",
-        link: "/participants-docs",
+        view: 'Документы',
+        viewEn: 'Documents',
+        link: '/participants-docs',
       },
       {
-        view: "Услуги для экспонентов",
-        viewEn: "Participants",
-        link: "/participants-services",
+        view: 'Услуги для экспонентов',
+        viewEn: 'Participants',
+        link: '/participants-services',
       },
     ],
   },
   {
-    view: "Посетителям",
+    id: 7,
+
+    view: 'Посетителям',
+    menu: true,
+
     info: [
       {
-        view: "Информация для посетителей",
-        link: "/visitors-info",
+        view: 'Информация для посетителей',
+        link: '/visitors-info',
       },
       {
-        view: "Зачем посещать",
-        link: "/visitors-visit",
+        view: 'Зачем посещать',
+        link: '/visitors-visit',
       },
     ],
   },
