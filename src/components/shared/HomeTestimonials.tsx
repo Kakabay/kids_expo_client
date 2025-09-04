5c38ddf53bfa92743b73c0c6bf8b4bc8c9fd95f9
Batyr <awbatyr@gmail.com>
2025-09-04 15:41:30 +0500

    build

--- src/components/shared/HomeTestimonials.tsx
+++ src/components/shared/HomeTestimonials.tsx
@@ -1,4 +1,5 @@
 import useEmblaCarousel from "embla-carousel-react";
+import { Quote } from "lucide-react";
 
 const HomeTestimonials = () => {
   const [emblaRef] = useEmblaCarousel();
@@ -8,18 +9,26 @@ const HomeTestimonials = () => {
       <h2 className="h2 mb-8 text-center">Title</h2>
 
       <div ref={emblaRef} className="embla overflow-hidden">
-        <div className="embla__container flex justify-center">
+        <div className="embla__container max-w-[710px] mx-auto gap-[20vw] flex justify-center">
           {[...Array(3)].map((_) => (
-            <article className="flex px-[18vw] flex-col text-center gap-3 flex-[0_0_100%] justify-center align-center">
+            <article className="relative flex flex-col text-center gap-3 flex-[0_0_100%] justify-center items-center">
+              <Quote
+                className="absolute left-0 rotate-180 bottom-14
+               text-text-interactive stroke-1 fill-text-interactive"
+              />
+              <Quote className="absolute right-0 bottom-2 text-text-interactive stroke-1 fill-text-interactive" />
+
               <img
                 src=""
                 alt=""
-                className="size-20 bg-black rounded-full mx-auto"
+                className="size-[100px] bg-black rounded-full"
               />
 
-              <h3 className="font-bold text-lg">Name</h3>
-              <h4 className="text-lg">Position</h4>
-              <p className="text-sm">
+              <div className="">
+                <h3 className="font-bold text-base">Name</h3>
+                <h4 className="text-base">Position</h4>
+              </div>
+              <p className="text-base text-text-secondary px-14">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed,
                 saepe, velit quia magnam ducimus hic dolorem, obcaecati itaque
                 nemo tempora ab id libero! Dolore vel laudantium sint suscipit
